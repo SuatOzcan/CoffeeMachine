f4328374f9a2d7994e669c5965ed4ba8c9f0b45a
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2023-11-07 15:26:50 +0300

    Start inserting coins.

--- app.py
+++ app.py
@@ -7,23 +7,38 @@ espresso = Espresso()
 cappuccino = Cappuccino()
 latte = Latte()
     
-def deduct_resources(coffee_type):
-    coffee_type_costs = coffee_type.give_costs()
-    for i in range(coffee_machine.resources.__len__()):
-        if i < (coffee_machine.resources.__len__() - 1):
-            coffee_machine.resources[i] = coffee_machine.resources[i] - coffee_type_costs[i]
-            continue
-        coffee_machine.resources[i] = coffee_machine.resources[i] + coffee_type_costs[i]
+def deduct_resources(coffee_type, inserted_money):
+
+    resource_checker = True
+
+    for i in range((coffee_machine.resources.__len__())):
+         if coffee_machine.resources[i] < coffee_type.costs[i]:
+              resource_checker = False
+
+    enough_money = inserted_money >= coffee_type.price
+
+    if resource_checker and enough_money:
+        for i in range(coffee_machine.resources.__len__()):
+            if i < (coffee_machine.resources.__len__() - 1):
+                coffee_machine.resources[i] = coffee_machine.resources[i] - coffee_type.costs[i]
+                continue
+            coffee_machine.resources[i] = coffee_machine.resources[i] + coffee_type.costs[i]
+            print(f'Enjoy your {coffee_type.name}!')
+    else:
+         print('Please insert more money.')
 
 prompt = user_input()
 while prompt != 'off':
     if prompt == 'report':
             print(f'Water: {coffee_machine.resources[0]}ml\nMilk: {coffee_machine.resources[1]}ml\nCoffee: {coffee_machine.resources[2]}g\nMoney: ${coffee_machine.resources[3]}')          
     elif prompt == 'espresso':
-        deduct_resources(espresso)
+        inserted_money = int(input('Please insert $4.\n'))
+        deduct_resources(espresso, inserted_money)
     elif prompt =='latte':
-        deduct_resources(latte)
+        inserted_money = int(input('Please insert $5.\n'))
+        deduct_resources(latte, inserted_money)
     elif prompt == 'cappuccino':
-        deduct_resources(cappuccino)
+        inserted_money = int(input('Please insert $2.\n'))
+        deduct_resources(cappuccino, inserted_money)
     
     prompt = user_input()
--- coffee_types.py
+++ coffee_types.py
@@ -1,34 +1,40 @@
 from coffee import Coffee
 
-class Cappuccino(Coffee):
-    def __init__(self):
-        super().__init__()
-        self.milk = 50
-        self.coffee = 12
-        self.price = 2
-    
-    def give_costs(self):
-        self.costs = [self.water, self.milk, self.coffee, self.price]
-        return self.costs
-
 class Espresso(Coffee):
     def __init__(self):
         super().__init__()
         self.milk = 2
         self.coffee = 100
         self. price = 4
-
+        self.name = 'espresso'
+        self.give_costs()
+        
     def give_costs(self):
         self.costs = [self.water, self.milk, self.coffee, self.price]
         return self.costs
-
+    
 class Latte(Coffee):
     def __init__(self):
         super().__init__()
         self.milk = 200
         self.coffee = 100
         self.price = 5
+        self.name = 'latte'
+        self.give_costs()
 
     def give_costs(self):
         self.costs = [self.water, self.milk, self.coffee,self.price]
+        return self.costs
+
+class Cappuccino(Coffee):
+    def __init__(self):
+        super().__init__()
+        self.milk = 50
+        self.coffee = 12
+        self.price = 2
+        self.name ='cappuccino'
+        self.give_costs()
+
+    def give_costs(self):
+        self.costs = [self.water, self.milk, self.coffee, self.price]
         return self.costs
